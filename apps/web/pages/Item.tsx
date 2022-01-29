2b47a81f75cdd423e4a4a84402e76f443d32e1b2
SAYEM5522 <md1040582@gmail.com>
2022-01-29 16:47:41 +0600

    Pages Add

--- apps/web/pages/Item.tsx
+++ apps/web/pages/Item.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import Link from "next/link";
 import { useRouter } from 'next/router';
-
-export const Item = () => {
+import React, { useCallback } from 'react';
+import styls from "./css/Item.module.css";
+const Item = () => {
+  const {query}=useRouter();
   const router=useRouter();
-  const onClick=React.useCallback(()=>{
-      router.push("/")
-  },[])
-  return <div>
-    <h1 onClick={onClick}>yoo</h1>
-  </div>;
+  const onClick:React.MouseEventHandler<HTMLHeadingElement>=useCallback((e)=>{
+},[])
+  return (
+    <div>
+      <h1 className={styls.item}>jeeeee</h1>
+      <p onClick={onClick}>{query.id}</p>
+    </div>
+  )
 };
+
+export default Item;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,20 @@
+import { useRouter } from "next/router";
 import { Button } from "ui";
-import { Item } from "./Item";
+import React, { useCallback } from "react";
+import Item from "./Item";
 
 export default function Web() {
+  const router=useRouter(); 
+  const onClick:React.MouseEventHandler<HTMLHeadingElement>=useCallback((e)=>{
+    
+      router.push("/Item?id=foo")
+  },[])
   return (
     <div>
-      <Item />
-      <Button />
+      <div  onClick={onClick}>
+      <Button  />
+      </div>
+     
     </div>
   );
 }
