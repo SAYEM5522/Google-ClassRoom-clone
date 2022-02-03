060bc880658e9ac89f99f1b2aebd965f57bc6723
SAYEM5522 <md1040582@gmail.com>
2022-02-03 19:51:43 +0600

    Class add

---
+++ apps/web/pages/Course/Class.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Class = () => {
+  return <div>
+    <h1>Class</h1>
+  </div>;
+};
+
+export default Class;
--- apps/web/pages/SideBar/SideBar.tsx
+++ apps/web/pages/SideBar/SideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from "../css/SideBar/SideBar.module.css"
 const SIdeBar = () => {
   return <div className={styles.sidebar}>
-    <h1>hello</h1>
+          <h1>hello</h1>
   </div>;
 };
 
