58937c72d61d6fbceee7b08c0cf7c899a37682a5
SAYEM5522 <md1040582@gmail.com>
2022-02-03 19:26:54 +0600

    SideBar Add

--- apps/web/pages/Header/Header.tsx
+++ apps/web/pages/Header/Header.tsx
@@ -1,29 +1,38 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../css/Header/Header.module.css"
 import { FiMenu } from "react-icons/fi";
 import{AiOutlinePlus} from "react-icons/ai";
 import {GrApps} from "react-icons/gr";
-
+import { useRouter } from 'next/router';
+import SIdeBar from '../SideBar/SideBar';
 const imageWidth:number=70
 const imageHight:number=70
 
 const Header = () => {
+  const [open,setOpen]=useState(false);
+  const router=useRouter()
+  const onClick:React.MouseEventHandler<SVGElement>=()=>{
+      setOpen(!open);
+      console.log(open);
+  }
   
   return (
   <div className={styles.header}>
+   
+    
     <div className={styles.header__left} >
-      <FiMenu size={22} className={styles.header__Icon}/>
+    {
+      open?<SIdeBar/>:null
+    }
+      <FiMenu size={22} className={styles.header__Icon} onClick={onClick}/>
       <Image
             src="/../public/logo.jpg"
             alt="Google"
             width={imageWidth}
             height={imageHight}
-  
             objectFit="contain"
             objectPosition="center"
-            
-
     />
     <p >Classroom</p>
     </div>
@@ -31,7 +40,6 @@ const Header = () => {
       <AiOutlinePlus size={21} style={{ marginRight:"1.9rem"}} />
       <GrApps size={21} style={{ marginRight:"1.6rem"}} />
        
-      {/* <div className={styles.Avator} > */}
       <Image 
        src="/../public/avator.jpg"
        alt="Google"
@@ -46,7 +54,6 @@ const Header = () => {
           margin-right:7px;
         }
       `}</style>
-      {/* </div> */}
 
     </div>
 
---
+++ apps/web/pages/SideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "../css/SideBar/SideBar.module.css"
+const SIdeBar = () => {
+  return <div className={styles.sidebar}>
+    <h1>hello</h1>
+  </div>;
+};
+
+export default SIdeBar;
