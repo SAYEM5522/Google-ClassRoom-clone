7695023b9771074681349b5385f5a66fbcd6a2a7
SAYEM5522 <md1040582@gmail.com>
2022-02-04 16:04:50 +0600

    ClassFeed add

--- apps/web/pages/Course/Class.tsx
+++ apps/web/pages/Course/Class.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ClassFeed from './ClassFeed';
 import CourseHeader from './CourseHeader';
 
 const Class = () => {
   return <div>
     <CourseHeader/>
+    <ClassFeed/>
   </div>;
 };
 
---
+++ apps/web/pages/Course/ClassFeed.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "../css/Course/ClassFeed.module.css"
+const ClassFeed = () => {
+  return <div className={styles.classfeed}>
+    <h1>ClassFeed</h1>
+  </div>;
+};
+
+export default ClassFeed;
