2fe6a7a30ea5298f43b759320963a95eb98829e1
SAYEM5522 <md1040582@gmail.com>
2022-02-04 15:31:41 +0600

    Header Distribute

--- apps/web/pages/Course/CourseHeader.tsx
+++ apps/web/pages/Course/CourseHeader.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from "../css/Course/CourseHeader.module.css";
 import { FiMenu } from "react-icons/fi";
+import Image from 'next/image';
+import { GrApps } from 'react-icons/gr';
 
 const CourseHeader = () => {
-  return <div className={styles.courseheader}>
+  return (
+<div className={styles.courseheader}>
     <div className={styles.courseheader__left}>
       <FiMenu size={22}/>
       <div className={styles.courseheader__left__name}>
@@ -12,12 +15,35 @@ const CourseHeader = () => {
       </div>
     </div>
     <div className={styles.courseheader__middle}>
+      <p>Stream</p>
+      <p>ClassWork </p>
+      <p>People</p>
+
 
     </div>
     <div className={styles.courseheader__right}>
-
+    <GrApps size={21} style={{ marginRight:"1.6rem"}} />
+       
+       <Image 
+        src="/../public/avator.jpg"
+        alt="Google"
+        width={30}
+        height={30}
+        objectFit='cover'
+        className='avatar'
+       />
+           <style jsx global>{`
+         .avatar {
+           border-radius: 50% !important;
+           
+         }
+       `}</style>
     </div>
-  </div>;
+  </div>
+  )
+
+   
+  
 };
 
 export default CourseHeader;
