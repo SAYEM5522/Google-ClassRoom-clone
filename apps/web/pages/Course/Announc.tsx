594b575236c751477a22531306fb6b5e0abb23d8
SAYEM5522 <md1040582@gmail.com>
2022-02-04 20:22:08 +0600

    Course Details Background Add

--- apps/web/pages/Course/Announc.tsx
+++ apps/web/pages/Course/Announc.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from "../css/Course/Announce.module.css"
 const Announc = () => {
   return <div className={styles.announce}>
-    <h1>Announce</h1>
   </div>;
 };
 
--- apps/web/pages/Course/Upcaming.tsx
+++ apps/web/pages/Course/Upcaming.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from "../css/Course/Upcomming.module.css"
 const Upcaming = () => {
   return <div className={styles.upcoming}>
-    <h1>upcomming</h1>
+        <p className={styles.upcoming__top}>Upcoming</p>
+        <p className={styles.upcoming__middle}>Woohoo, no work due soon!</p>
+        <p className={styles.upcoming__list}>View all</p>
   </div>;
 };
 
