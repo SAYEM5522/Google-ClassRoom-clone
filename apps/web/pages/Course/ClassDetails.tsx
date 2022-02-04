5b3634e360cbc345f6a1eca2825740aef31eb87b
SAYEM5522 <md1040582@gmail.com>
2022-02-04 16:44:26 +0600

    ClassDetails Add

--- apps/web/pages/Course/ClassDetails.tsx
+++ apps/web/pages/Course/ClassDetails.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import styles from "../css/Course/ClassDetails.module.css"
 const ClassDetails = () => {
-  return <div>
-    <h1>upcomming</h1>
+  return <div className={styles.classdetails}>
+        <div className={styles.classdetails__name}>
+          <h1>CSE311.8 Spring 2022</h1>
+          <h4>8</h4>
+        </div>
   </div>;
 };
 
--- apps/web/pages/Course/ClassFeed.tsx
+++ apps/web/pages/Course/ClassFeed.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from "../css/Course/ClassFeed.module.css"
+import ClassDetails from './ClassDetails';
 const ClassFeed = () => {
   return <div className={styles.classfeed}>
-    <h1>ClassFeed</h1>
+            <ClassDetails/>
   </div>;
 };
 
