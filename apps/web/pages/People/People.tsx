f2ee2c1247ebbe5bbbb55ac03bd94e563c48b894
SAYEM5522 <md1040582@gmail.com>
2022-02-06 19:49:31 +0600

    Teacher Component Update

--- apps/web/pages/People/People.tsx
+++ apps/web/pages/People/People.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import CourseHeader from '../Course/CourseHeader';
 import styles from "../css/People/People.module.css";
+import ClassMates from './ClassMates';
+import Teacher from './Teacher';
 const People = () => {
   return <div>
    <CourseHeader/>
    <div className={styles.people__list}>
-
+      <Teacher/>
+      <ClassMates/>
    </div>
   </div>;
 };
--- apps/web/pages/People/Teacher.tsx
+++ apps/web/pages/People/Teacher.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
 import React from 'react';
+import { CgMail } from 'react-icons/cg';
 import styles from "../css/People/Teacher.module.css";
 const Teacher = () => {
-  return <div>
-    <h1>teacher</h1>
+  return <div className={styles.teacher}>
+    <h1>Teachers</h1>
+    <div className={styles.teacher__line}/>
+    <div className={styles.teacher__bottom}>
+      <Image
+      src={"/../public/avt4.jpg"}
+      alt="Google"
+      width={32}
+      height={32}
+      objectFit='cover'
+      className='teacher__avatar'
+      />
+      <CgMail size={26} className={styles.teacher__icon} />
+      
+    </div>
   </div>;
 };
 
