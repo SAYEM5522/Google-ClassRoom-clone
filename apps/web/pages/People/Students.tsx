fbda62527895197483c38f50de6d0852481be21f
SAYEM5522 <md1040582@gmail.com>
2022-02-06 22:06:22 +0600

    Student Component Update

--- apps/web/pages/People/Students.tsx
+++ apps/web/pages/People/Students.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
 import React from 'react';
+import styles from "../css/People/Students.module.css";
+import { CgMail } from 'react-icons/cg';
 
 const Students = () => {
-  return <div>
-    <h1>students</h1>
+  return <div className={styles.students}>
+        <div className={styles.students__details}>
+          <Image 
+          src={"/../public/avt3.jpg"}
+          alt="Google"
+          width={32}
+          height={32}
+          objectFit='cover'
+          className='students__avatar'
+          />
+          <p className={styles.students__name}>Semonti Barua 2012758042</p>
+          <CgMail size={26}/>
+        
+        </div>
   </div>;
 };
 
