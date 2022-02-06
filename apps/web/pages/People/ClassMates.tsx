110ec7e88c281523fb9dbac7392b46a6a107c035
SAYEM5522 <md1040582@gmail.com>
2022-02-06 20:10:19 +0600

    Student Component Add

--- apps/web/pages/People/ClassMates.tsx
+++ apps/web/pages/People/ClassMates.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from "../css/People/ClassMates.module.css";
+import Students from './Students';
 const ClassMates = () => {
-  return <div>
-    <h1>classmats</h1>
+  return <div className={styles.classmates}>
+      <div className={styles.classmates__top}>
+          <h1>Classmates</h1>
+          <p>36 students</p>
+      </div>
+      <div className={styles.classmates__line}/>
+      <Students/>
   </div>;
 };
 
---
+++ apps/web/pages/People/Students.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Students = () => {
+  return <div>
+    <h1>students</h1>
+  </div>;
+};
+
+export default Students;
