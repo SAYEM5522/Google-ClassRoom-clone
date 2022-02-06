6f12aab68d8b171680943b7674c4e2c0100e5ccd
SAYEM5522 <md1040582@gmail.com>
2022-02-06 17:10:23 +0600

    ...

--- apps/web/pages/SideBar/SideBar.tsx
+++ apps/web/pages/SideBar/SideBar.tsx
@@ -4,6 +4,7 @@ import CourseName from './CourseName';
 import SideBarTop from './SideBarTop';
 import {CgHomeAlt} from "react-icons/cg";
 import {BiCalendarAlt} from "react-icons/bi";
+import {BsCardChecklist} from "react-icons/bs";
 
 const SIdeBar = ({setOpen}) => {
   return <div className={styles.sidebar}>
@@ -15,12 +16,18 @@ const SIdeBar = ({setOpen}) => {
           </div>
           <div className={styles.sidebar__middle}>
                 <h3 className={styles.sidebar__middle__top}>Enrolled</h3>
+                <div className={styles.sidebar__middle__todo}>
+                        <BsCardChecklist size={21.5}/>
+                        <h3>To-do</h3>
+                </div>
                 <CourseName name={"CSE311.8 Spring 2022"} section={8} background={"#007b83"}/>
                 <CourseName name={"ECO101.18 Spring 2022"} section={10} background={"#1967d2"}/>
                 <CourseName name={"MAT361.11 Spring 2022"} section={11}  background={"#202124"}/>
                 <CourseName name={"CSE311.8 Spring 2022"} section={18}  background={"#137333"}/>
                 <CourseName name={"CSE311.8 Spring 2022"} section={21}  background={"#1967d2"}/>
                 <CourseName name={"BEN201.8 Spring 2022"} section={20}  background={"#137333"}/>
+                <CourseName name={"BEN201.8 Spring 2022"} section={20}  background={"#137333"}/>
+
 
           </div>
           <div className={styles.sidebar__bottom}>
