3c3cae9502a6ba7acb1e5def25f3f25baeee3a49
SAYEM5522 <md1040582@gmail.com>
2022-02-10 20:26:09 +0600

    FeedMiddle And FeedBottom Complete

--- apps/web/pages/Course/FeedBottom.tsx
+++ apps/web/pages/Course/FeedBottom.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image';
 import React from 'react';
-
+import styles from "../css/Course/FeedBottom.module.css"
+import {BiSend} from "react-icons/bi"
 const FeedBottom = () => {
-  return <div>
-    <h1>feedbottom</h1>
+  return <div className={styles.feedbottom}>
+            <div className={styles.feedbottom__line}/>
+            <div className={styles.feedbottom__comment}>
+            <Image
+      src={"/../public/avt6.jpg"}
+      alt="Google"
+      width={32}
+      height={32}
+      objectFit='cover'
+      className='feedbottom__comment__avatar'
+      />
+      <div className={styles.feedbottom__comment__section}>
+      <input placeholder='Add class comment...' className={styles.feedbottom__comment__input}/>
+        <BiSend className={styles.feedbottom__comment__icon} size={24}/>
+      </div>
+            </div>
   </div>;
 };
 
